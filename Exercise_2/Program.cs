8d77ae3b7d205e8b4e092219ef83189cfd69699f
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2022-11-02 15:51:56 +0700

    Create method

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace Exercise_2
 {
-    internal class Program
+    class Program
     {
+        //deklarasi array int dengan ukuran 40
+        private int[] a = new int[40];
+        //deklarasi variabel int array
+        private int n;
+        //fungsi untuk menerima ukuran array
+        public void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Masukkan jumlah element array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 40)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 40 elemen.\n");
+
+            }
+        }
         static void Main(string[] args)
         {
         }
